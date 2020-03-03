0d121eb96f91bfb1040f29dd73ccb408ec6ef7a7
Siarhei Zaretski <sphincs.by@gmail.com>
2020-03-04 01:00:23 +0300

    Resolve lesson06 task02 variant02

---
+++ src/jc01_2020/zaretski/lesson06/task02/variant02/Main.java
@@ -0,0 +1,27 @@
+package jc01_2020.zaretski.lesson06.task02.variant02;
+
+public class Main {
+    public static void main(String[] args) {
+        Atm atm = new Atm(5, 2, 1);
+        System.out.print("Запрашиваемая сумма 110 - ");
+        atm.withdraw(110);
+//        System.out.print("Запрашиваемая сумма 10 - ");
+//        atm.withdraw(10);
+//        System.out.print("Запрашиваемая сумма 100 - ");
+//        atm.withdraw(100);
+        System.out.print("Запрашиваемая сумма 100 - ");
+        atm.withdraw(100);
+        System.out.print("Запрашиваемая сумма 100 - ");
+        atm.withdraw(100);
+        System.out.print("Запрашиваемая сумма 50 - ");
+        atm.withdraw(50);
+//        System.out.print("Запрашиваемая сумма 630 - ");
+//        atm.withdraw(630);
+//        System.out.print("Запрашиваемая сумма 855 - ");
+//        atm.withdraw(855);
+//        System.out.print("Запрашиваемая сумма 1450 - ");
+//        atm.withdraw(1450);
+//        System.out.print("Запрашиваемая сумма 2890 - ");
+//        atm.withdraw(2890);
+    }
+}
